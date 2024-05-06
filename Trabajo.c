6455ea95c151108bdd524b36cae6ec70dd689ea6
Alfredo Becerra <119506519+AlfredoBE@users.noreply.github.com>
2024-05-06 18:56:00 -0400

    Trabajo final

--- Trabajo.c
+++ Trabajo.c
@@ -1,3 +1,12 @@
+/*
+Autores:
+
+Alfredo Becerra
+Cristobal Ramirez
+
+Asignatura: Sistemas Distribuidos
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,6 +15,8 @@
 #define DIM 20
 #define N_THREADS 8
 
+
+//Funcion de distancia euclidiana
 double euclideanDistance(double v1[], double v2[], int dim)
 {
     double sum = 0.0;
@@ -20,10 +31,14 @@ double euclideanDistance(double v1[], double v2[], int dim)
 int main()
 {
 
-    // Abre el archivo en modo lectura
+    // Se realiza lectura de archivo
     FILE *archivo;
 
-    archivo = fopen("test60.txt", "r");
+    char nombre_archivo[100]; // Declara un array para almacenar el nombre
+    printf("Ingrese nombre del archivo: ");
+    scanf("%s", nombre_archivo); // Captura la cadena desde el teclado para ser usado como el nombre
+
+    archivo = fopen(nombre_archivo, "r");
 
     if (archivo == NULL)
     {
@@ -31,7 +46,7 @@ int main()
         return 1;
     }
 
-    // Declara un array para almacenar los datos
+    // Declara un array para almacenar los datos del archivo
     double *datos;
     int capacidad = 10; // Capacidad inicial del array
     int num_datos = 0;  // Número actual de datos
@@ -43,7 +58,7 @@ int main()
         return 1;
     }
 
-    // Lee los datos del archivo y los almacena en el array
+    // Lee los datos del archivo y los almacena en el array datos
     double dato;
     while (fscanf(archivo, "%lf", &dato) == 1)
     {
@@ -67,13 +82,14 @@ int main()
     // Cierra el archivo
     fclose(archivo);
 
+    // Definimos variables a usar para realizar las operaciones
     int N = datos[0];
     double M = datos[1];
     double alfa = 0.4;
     double criterio = (M * alfa);
     double distancias;
 
-    // Leer y almacenar los componentes de cada vector
+    // Leer y almacenar los componentes de cada vector de un array 
     double vectores[N][DIM];
     for (int i = 0; i < N; i++)
     {
@@ -83,43 +99,76 @@ int main()
         }
     }
 
-    // Crear la lista de elementos y de pivotes, para ser llenada mas adelante con el criterio
+    // Crear la lista de elementos (no pivotes) y de pivotes, para ser llenada mas adelante con el criterio
     int pivotes[N];
     int num_pivotes = 1;
     pivotes[0] = 0;
 
-    int elementos[N];
+    int no_pivotes[N];
+    int num_no_pivotes = 0;
     // Aca empezamos a usar hilos, se reparten el trabajo de calcular la distancia euclidiana
     // para descubrir los pivotes
     omp_set_num_threads(N_THREADS);
-#pragma omp parallel for shared(vectores, criterio)
+
+    // parallel for shared indica que para el proximo for los hilos se dividiran el trabajo de ejecutar
+    // el bucle que contiene el calculo de distancia euclidiana, shared especificamente indica que existe
+    // transferencia de informacion entre hilos para evitar algun problema
+#pragma omp parallel for shared(vectores, criterio, num_pivotes, pivotes)
     // Primero hacemos un barrido desde el P0 para encontrar los pivotes
     for (int i = 0; i < N; i++)
     {
         int j;
         for (j = 0; j < num_pivotes; j++)
         {
-            distancias = euclideanDistance(vectores[pivotes[j]], vectores[i], DIM);
+            distancias = euclideanDistance(vectores[pivotes[j]], vectores[i], DIM); // Llamado a la funcion que calcula la distancia euclidiana entre dos vectores i j
             if (distancias < (criterio))
-            { // Si no cumple el criterio de estar al menos a M*a
-            break;
+            {
+                if(i==0 && j==0){ // Condicion para evitar que P0 sea añadido como elemento no pivote debido a que su distancia con sigo mismo es cero
+                    break;
+                }
+        // Si no cumple el criterio de estar al menos a distancia M*a se añade a lista de elementos (no pivotes)
+                #pragma omp critical
+                {   
+                    no_pivotes[num_no_pivotes++] = i;
+                }
+                break;
             }
         }
         if (j == num_pivotes)
         {
             pivotes[num_pivotes++] = i;
         }
-        // Si cumple el criterio se añade a la lista
+        // Si cumple el criterio se añade a la lista de pivotes
     }
 
-printf("Lista de pivotes:\n");
-for (int i = 0; i < num_pivotes; i++) {
-    printf("Pivote %d: vector %d\n", (i+1), pivotes[i]);
-}
+    //Ahora se debe realiza una matriz distancia entre los pivotes y los elementos
+    double matriz_distancia[num_pivotes][num_no_pivotes];
+    #pragma omp parallel for shared(num_pivotes, num_no_pivotes, vectores, pivotes, matriz_distancia)
+    for (int i = 0; i < (num_pivotes); i++)
+    {
+        for (int j = 0; j < num_no_pivotes; j++)
+        {
+            matriz_distancia[i][j] = euclideanDistance(vectores[pivotes[i]], vectores[no_pivotes[j]] ,DIM);
+        }
+        
+    }
+    // Se muestra en pantalla la matriz distancia, las distancias separadas por un espacio
+    // y se indica a que pivote corresponde para mayor claridad al leer
+    printf("--------------------------------------------------------------------------Matriz de distancias--------------------------------------------------------------------------\n");
+    for (int k = 0; k < num_pivotes; k++) {
+        printf("\nPivote %d:\n", k); // Imprimir el número del pivote
+        for (int l = 0; l < num_no_pivotes; l++) {
+            printf(" %lf ", matriz_distancia[k][l]); // Imprimir la distancia
+        }
+        printf("\n");
+    }
 
-// Libera la memoria asignada al array
-free(datos);
-free(vectores);
+    // Libera la memoria asignada a los array
+    free(datos);
+    free(vectores);
+    free(pivotes);
+    free(no_pivotes);
+    free(matriz_distancia);
 
-return 0;
+    return 0;
 }
